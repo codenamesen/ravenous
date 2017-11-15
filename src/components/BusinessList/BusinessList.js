7982f85e3b37af377f76fce01df263c25ab4fe12
Sandip Sen <umel@protonmail.com>
2017-11-15 18:28:29 +0530

    Add Business Component

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.component{
+  render() {
+    return (
+      <div class="BusinessList">
+      <!-- Add Business Component here -->
+      <Business />
+      <!-- Add Business Component here -->
+      <Business />
+      <!-- Add Business Component here -->
+      <Business />
+      <!-- Add Business Component here -->
+      <Business />
+      <!-- Add Business Component here -->
+      <Business />
+      <!-- Add Business Component here -->
+      <Business />
+</div>
+    );
+  }
+}
